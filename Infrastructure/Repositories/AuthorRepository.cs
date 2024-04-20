1ff45ad1d77b49f33db2c279be9f8c6ed5f1698c
Frida <fridapeersson@gmail.com>
2024-04-20 19:07:53 +0200

    .

--- Infrastructure/Repositories/AuthorRepository.cs
+++ Infrastructure/Repositories/AuthorRepository.cs
@@ -4,43 +4,42 @@ using Microsoft.EntityFrameworkCore;
 using System.Diagnostics;
 using System.Linq.Expressions;
 
-namespace Infrastructure.Repositories
+namespace Infrastructure.Repositories;
+
+public class AuthorRepository : BaseRepo<AuthorsEntity>
 {
-    public class AuthorRepository : BaseRepo<AuthorsEntity>
+    private readonly DataContext _context;
+    public AuthorRepository(DataContext context) : base(context)
     {
-        private readonly DataContext _context;
-        public AuthorRepository(DataContext context) : base(context)
-        {
-            _context = context;
-        }
+        _context = context;
+    }
 
-        public override async Task<IEnumerable<AuthorsEntity>> GetAllAsync()
+    public override async Task<IEnumerable<AuthorsEntity>> GetAllAsync()
+    {
+        try
         {
-            try
+            return await _context.Authors
+                //.Include(i => i.Courses)
+                .ToListAsync();
+        }
+        catch (Exception ex) { Debug.WriteLine(ex); }
+        return null!;
+    }
+
+    public override async Task<AuthorsEntity> GetOneAsync(Expression<Func<AuthorsEntity, bool>> predicate)
+    {
+        try
+        {
+            var entity = await _context.Authors
+                //.Include(i => i.Courses)
+                .FirstOrDefaultAsync(predicate);
+
+            if (entity != null)
             {
-                return await _context.Authors
-                    //.Include(i => i.Courses)
-                    .ToListAsync();
+                return entity;
             }
-            catch (Exception ex) { Debug.WriteLine(ex); }
-            return null!;
         }
-
-        public override async Task<AuthorsEntity> GetOneAsync(Expression<Func<AuthorsEntity, bool>> predicate)
-        {
-            try
-            {
-                var entity = await _context.Authors
-                    //.Include(i => i.Courses)
-                    .FirstOrDefaultAsync(predicate);
-
-                if (entity != null)
-                {
-                    return entity;
-                }
-            }
-            catch (Exception ex) { Debug.WriteLine(ex); }
-            return null!;
-        }
+        catch (Exception ex) { Debug.WriteLine(ex); }
+        return null!;
     }
 }
